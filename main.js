7c1cecbc97958be8e5f327b6271e59ec522f6ecf
the7ven <nkwelcorneille7@gmail.com>
2024-07-25 03:03:14 +0200

    js scroll

---
+++ main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const elements = document.querySelectorAll('.hidden');
+  
+    const revealOnScroll = () => {
+      const windowHeight = window.innerHeight;
+      elements.forEach(element => {
+        const elementTop = element.getBoundingClientRect().top;
+        if (elementTop < windowHeight - 100) {
+          element.classList.add('reveal');
+        }
+      });
+    };
+  
+    window.addEventListener('scroll', revealOnScroll);
+    revealOnScroll();  // Check for reveal elements on page load
+  });
